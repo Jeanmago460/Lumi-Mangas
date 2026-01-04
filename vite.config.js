96185ea5093431e1869843143343d26605d13331
Jeanmago460 <claudiojean345@gmail.com>
2026-01-04 19:38:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Este selo força o sistema a reconhecer o código JSX mesmo em arquivos .js
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
-  },
   esbuild: {
-    loader: "jsx",
+    loader: 'jsx',
     include: /src\/.*\.jsx?$/,
     exclude: [],
   },
