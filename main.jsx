12a4d4e1d3ec52fe046ff43d21c924c1b0bdff38
Jeanmago460 <claudiojean345@gmail.com>
2026-01-05 00:47:09 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Este é o comando que injeta o React no seu HTML
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
-
