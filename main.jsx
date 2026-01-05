2cb16397342ef54e050ee09dae66737c88689f14
Jeanmago460 <claudiojean345@gmail.com>
2026-01-05 01:05:39 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Este comando localiza o 'root' no index.html e injeta o App.jsx lá dentro
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>)
+  </React.StrictMode>
+)
